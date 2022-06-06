c08077f9d59ed0fea2a4c164273c95a2f609d07d
Cécile Deshayes-Guinard <cecile.deshayes.guinard@gmail.com>
2022-06-06 20:54:09 +0200

    shuffle-card=ok

--- window.js
+++ window.js
@@ -1,12 +1,14 @@
 /* PHASE 1: MELANGE DES IMAGES ET DEBUT DE JEU */
 
-let arraySelected = gallery[0]; // j'oblige la selection de la première gallerie dans data.js
+let level = 0
+
+let arraySelected = gallery[level]; // j'oblige la selection de la première gallerie dans data.js
 console.log(arraySelected);
 
 /* première étape : le mélange des images */
 
 
-function objectSelector () { // un objet est choisi au hasard dans la gallerie selectionnée de data.js
+function shuffle() { // un objet est choisi au hasard dans la gallerie selectionnée de data.js
   arraySelected.sort(function(a, b) { // .sort permet le tri entre deux éléments qui se suivent et donc de repositionner sur toute la longeur de l'array chaque éléments par rapport à sont précedent
     // Math.random() // ]0; 1[, ex: 0.123456
 
@@ -18,13 +20,44 @@ function objectSelector () { // un objet est choisi au hasard dans la gallerie s
   });
 };
 
-arraySelected = objectSelector(arraySelected); // je récupère la gallerie mélangée
+shuffle(); // tu as melange
+
 
 
 /* le mélange se fait mais sans projection dans notre HTML */
 
 
-arraySelected = Array.from(document.querySelectorAll('img')); // [ <img>, <img>, ... ]
+play.onclick = function () {
+  console.log(arraySelected) // au click on affiche les images coté back
+};
+
+// ICI il y a un problem car tu "ecrases" arraySelected  oui 
+
+const allImages = Array.from(document.querySelectorAll('table img')); // [ <img>, <img>, ... ]
+
+//
+// Parcourir chacune des images de allImages
+//   - pour chacune: mettre a jour son src avec l'image de arraySelected
+//
+// Changer chaque images de allImages par l'image de arraySelected
+
+
+/*
+arraySelected:
+
+[
+  { title: "trotroCirque", image: "trotroLogo.png", backImage: "trotroDosCarte.png"},
+  { title: "trotroCirque", image: "trotroLogo.png", backImage: "trotroDosCarte.png"},
+  { title: "trotroAccordeon", image: "trotroAccordeon.png", backImage: "trotroDosCarte.png"},
+]
+*/
+
+allImages.forEach(function (myimage, index) {
+  // myimage: <img>
+  
+  myimage.src = '/images/' + arraySelected[index].image; // ok 
+})
+
 let html = '';
 
 
@@ -33,23 +66,23 @@ let html = '';
 
 let backPicture; // je déclare une variable pour la première image cliquée
 let pairsCount = 0;
-
 let picture1; // la carte cliquée
 let picture2; // anciennement picture1 qui devient picture2 dès la seconde image cliquée
 
-let win; // effet spécial si paire trouvée
+let result; // effet spécial si paire trouvée
+result = document.querySelector('span');
 
 
-arraySelected.forEach(backPicture => { // on créer un évenement valable pour chaque image de backPictures
+
+
+allImages.forEach(backPicture => { // on créer un évenement valable pour chaque image de backPictures
   // backPicture est une <img>
   backPicture.onclick = function () { // l'évènement est généré par un click
     
     backPicture.classList.add('turned') // on ajoute la class 'turned' à l'image cliquée
-
     //
     // max 2 cartes
     //
-
     picture1 = picture2 // <img>
     picture2 = backPicture // <img>
 
@@ -65,6 +98,9 @@ arraySelected.forEach(backPicture => { // on créer un évenement valable pour c
       if (picture2.src === picture1.src) {
         console.log('win');
         pairsCount++;
+        // effet win au click avec remove lost si existe
+        result.classList.add('flashWin');
+        result.classList.remove('flashLost');
         // bloquer les 2 cartes avec la class 'validate' et supprimer la class 'turned' qui ne sert que temporairement
         picture1.classList.replace('turned','validate');
         picture2.classList.replace('turned', 'validate');
@@ -72,10 +108,13 @@ arraySelected.forEach(backPicture => { // on créer un évenement valable pour c
         picture2 = ''; // je supprime la picture2 pour repartir à 0 (autrement, le jeu recommance avec une backPicture lors du premier click)
       } 
         else {
-          console.log('loose');
+          console.log('lost');
           // remettre les 2 cartes a 0 (seulement celle ayant la class 'turned', validate n'est pas touché)
           picture2.classList.remove('turned');
           picture1.classList.remove('turned');
+          // effet lost au click avec remove win si existe
+          result.classList.add('flashLost');
+          result.classList.remove('flashWin');
           picture1 = ''; // je supprime la picture1 pour repartir à 0 (autrement, le jeu recommance avec une backPicture lors du premier click)
           picture2 = ''; // je supprime la picture2 pour repartir à 0 (autrement, le jeu recommance avec une backPicture lors du premier click)
         }

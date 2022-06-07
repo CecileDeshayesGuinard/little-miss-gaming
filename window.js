bd1ff00ed203b52e9491e3e1999c5c9b2ff8512b
Cécile Deshayes-Guinard <cecile.deshayes.guinard@gmail.com>
2022-06-07 02:22:39 +0200

    rest-levelUp

--- window.js
+++ window.js
@@ -12,22 +12,31 @@ function shuffle() { // un objet est choisi au hasard dans la gallerie selection
   });
 };
 
+/* PHASE 2 : REMPLACER LES IMAGES INITIALES PAR CELLES MELANGEES */
+
 function updateTableCards() {
+
   //
   // Parcourir chacune des images de allImages
-  //   - pour chacune: mettre a jour son src avec l'image de arraySelected
-  //
   // Changer chaque images de allImages par l'image de arraySelected
+  //
 
-  allImages.forEach(function (myimage, index) {
+  allImages.forEach(function (myImage, index) { 
     // myimage: <img>
-    
-    myimage.src = '/images/' + arraySelected[index].image; // ok 
+    myImage.classList.remove('validate'); // j'enlève la classe 'validate' chaque MAJ de mon tableau car ce cas n'arrive que par un rest ou le chargement d'un nouveau jeu
+    myImage.classList.remove('turned');
+    picture1 = '';
+    picture2 = '';
+    pairsCount = 0;
+    myImage.src = '/images/' + arraySelected[index].image; // on remplace par les images triés dans mes cartes
   })
 }
 
+
+/* DECLARATION DE VARIABLE REUTILISEEES */
+
 let level = 0
-let arraySelected;
+let arraySelected = gallery[0];
 const allImages = Array.from(document.querySelectorAll('table img')); // [ <img>, <img>, ... ]
 
 
@@ -36,23 +45,32 @@ let picture1; // la carte cliquée
 let picture2; // anciennement picture1 qui devient picture2 dès la seconde image cliquée
 
 let result; // effet spécial si paire trouvée
-result = document.querySelector('span');
+result = document.querySelector('#cercle');
+
+
+
 
 function start() {
   arraySelected = gallery[level]; // j'oblige la selection de la première gallerie dans data.js
   console.log(arraySelected);
 
-  /* première étape : le mélange des images */
-  shuffle(); // tu as melange
+  // on re-mélante les images par appel de la function shuffle
+  shuffle();
+
+  // on rechange les images de bases par celles mélangées par la function shuffle
+  updateTableCards()
+}
+
+/*
+function levelUp () {
+  arraySelected = gallery[level+1];
+
+  shuffle()
 
   updateTableCards()
-
-  // reset de toutes les classes turned
-  // TODO
-
-  // reset picture1/picture2
-  // reset pairsCount
 }
+*/
+
 
 /* PHASE 2: CLICK SUR IMAGE */
 
@@ -79,7 +97,7 @@ allImages.forEach(backPicture => { // on créer un évenement valable pour chaqu
     if (picture1) {
         
       if (picture2.src === picture1.src) {
-        console.log('win');
+        console.log('pair found');
         pairsCount++;
         // effet win au click avec remove lost si existe
         result.classList.add('flashWin');
@@ -91,7 +109,7 @@ allImages.forEach(backPicture => { // on créer un évenement valable pour chaqu
         picture2 = ''; // je supprime la picture2 pour repartir à 0 (autrement, le jeu recommance avec une backPicture lors du premier click)
       } 
         else {
-          console.log('lost');
+          console.log('mistake');
           // remettre les 2 cartes a 0 (seulement celle ayant la class 'turned', validate n'est pas touché)
           picture2.classList.remove('turned');
           picture1.classList.remove('turned');
@@ -124,7 +142,7 @@ allImages.forEach(backPicture => { // on créer un évenement valable pour chaqu
 });
 
 play.onclick = function () {
-  console.log(arraySelected) // au click on affiche les images coté back
+  console.log(arraySelected) // au click on mélange les images et on reset le jeu
 
   start()
 };
